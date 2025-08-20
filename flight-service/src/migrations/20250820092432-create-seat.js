769688a578743e4d13c28a3ab047ada672627e8c
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2025-08-20 15:46:36 +0530

    Added seat model and associated with Airplanes

---
+++ flight-service/src/migrations/20250820092432-create-seat.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const { Enums } = require("../utils");
+const { BUSINESS, ECONOMY, FIRST_CLASS, PREMIUM_ECONOMY } = Enums.SEATTYPE;
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Seats", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      airplaneId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Airplanes",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+      },
+      row: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      col: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      seatType: {
+        type: Sequelize.ENUM,
+        values: [ECONOMY, BUSINESS, FIRST_CLASS, PREMIUM_ECONOMY],
+        defaultValue: ECONOMY,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Seats");
+  },
+};
---
+++ flight-service/src/utils/enums.js
@@ -0,0 +1,10 @@
+const SEATTYPE = {
+  BUSINESS: "business",
+  ECONOMY: "economy",
+  FIRST_CLASS: "first-class",
+  PREMIUM_ECONOMY: "premium-economy",
+};
+
+module.exports = {
+  SEATTYPE,
+};
