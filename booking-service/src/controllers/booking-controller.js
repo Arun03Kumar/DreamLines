1a051e9420b789090ed3a6b100daebe3f2aea744
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2025-08-21 16:25:11 +0530

    Added idempotency key/api logic in controller

--- booking-service/src/controllers/booking-controller.js
+++ booking-service/src/controllers/booking-controller.js
@@ -1,5 +1,7 @@
 const { BookingService } = require("../services");
 
+const inMemDb = {};
+
 async function createBooking(req, res) {
   try {
     console.log(req.body);
@@ -16,12 +18,21 @@ async function createBooking(req, res) {
 
 async function makePayment(req, res) {
   try {
-    console.log(req.body);
+    // console.log(req.body);
+    const idempotencyKey = req.headers["x-idempotency-key"];
+    if (!idempotencyKey) {
+      throw new Error("Idempotency key is missing");
+    }
+
+    if (inMemDb[idempotencyKey]) {
+      throw new Error("Duplicate payment");
+    }
     const response = await BookingService.makePayment({
       bookingId: req.body.bookingId,
       userId: req.body.userId,
       totalCost: req.body.totalCost,
     });
+    inMemDb[idempotencyKey] = idempotencyKey;
     return res.status(200).json(response);
   } catch (error) {
     return res.status(500).json({ error: error.message });
